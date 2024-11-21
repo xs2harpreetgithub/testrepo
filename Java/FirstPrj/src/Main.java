7bcfc6b1c54edb1d2967b2a76ce40ac7ecfcc538
Harpreet Singh <harpreetsingh@Harpreets-MBP.lan>
2024-11-20 20:27:09 -0700

    Java repo added

---
+++ Java/FirstPrj/src/Main.java
@@ -0,0 +1,25 @@
+import java.net.SocketOption;
+import java.util.ArrayList;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
+        // to see how IntelliJ IDEA suggests fixing it.
+        System.out.println("Hello and welcome!");
+
+        ArrayList list = new ArrayList();
+        list.add("1");
+        list.add(1);
+
+        list.forEach(n-> System.out.println(n));
+
+        (int a )-> {System.out.println("Harpreet");}
+       /* for (int i = 1; i <= 5; i++) {
+            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
+            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
+            System.out.println("i = " + i);
+        }*/
+    }
+}
